e8b64f7e67e5fd74e87e547aab179d81828caa98
migueel15 <miguelangeldorado10@gmail.com>
2024-07-01 19:13:34 +0200

    add: test objects in renderer

--- src/engine/renderer.cpp
+++ src/engine/renderer.cpp
@@ -1,22 +1,31 @@
 #include "engine/renderer.hpp"
 #include "engine/mesh.hpp"
+#include "engine/object.hpp"
 #include <vector>
 
 Renderer::Renderer() {}
 
 void Renderer::update() {
   clearScreen();
-  std::vector<Vertex> vertices = {{
-                                      .position = {-0.5, 0.0, 0.0},
-                                  },
-                                  {
-                                      .position = {0.0, 0.5, 0.0},
-                                  },
-                                  {
-                                      .position = {0.5, 0.0, 0.0},
-                                  }};
+  std::vector<Vertex> vertices = {
+      {.position = {-0.5, 0.0, 0.0}},
+      {.position = {0.0, 0.5, 0.0}},
+      {.position = {0.5, 0.0, 0.0}},
+  };
+
+  std::vector<Vertex> vert2 = {{.position = {-0.7, 0.5, 0.0}},
+                               {.position = {-0.7, 1.0, 0.0}},
+                               {.position = {-0.1, 0.75, 0.0}}};
+
   Mesh mesh(vertices);
-  mesh.draw();
+  Shader shader("../assets/shaders/default.vs", "../assets/shaders/default.fs");
+  Mesh mesh2(vert2);
+
+  Object ob1(mesh, shader);
+  Object ob2(mesh2, shader);
+
+  ob1.draw();
+  ob2.draw();
 }
 
 void Renderer::clearScreen() {
